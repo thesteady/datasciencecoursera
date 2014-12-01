b2eace3eefc534b7d5b4d555c183a0dd2435ed83
Laura Steadman <lauramsteadman@gmail.com>
2014-11-30 17:25:24 -0700

    Fix a dumb rearrangement

--- R_Programming/assignment_3/best.R
+++ R_Programming/assignment_3/best.R
@@ -1,12 +1,12 @@
 best <- function(state, outcome) {
+	data <- read.csv('outcome-of-care-measures.csv', colClasses='character')
+
 	if( !stateValid(state, data) ) {
 		stop('invalid state')
 	}
 	if( !outcomeValid(outcome) ) {
 		stop('invalid outcome')
 	}
-	
-	data <- read.csv('outcome-of-care-measures.csv', colClasses='character')
 	outcomeColName <- generateColName(outcome)			  
 
 	## subset the data to the state in question
